b0453cc61aafa82975c47a9ee11b9285e7197807
Leondvlpr <leondvlpr@gmail.com>
2026-01-31 18:47:37 -0500

    fix: fix UpdateStockProductService mapping and execution logic

--- src/products/application/services/update-stock-product.service.ts
+++ src/products/application/services/update-stock-product.service.ts
@@ -2,16 +2,18 @@ import { UpdateStockMapper } from 'src/products/infrastructure/mappers/update-st
 import { UpdateStockProductUseCase } from '../use-cases/update-stock-product.use-case';
 import { UpdateStockOutputDto } from '../output/update-stock-output.dto';
 import { UpdateStockDto } from 'src/products/infrastructure/dto/update-stock.dto';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class UpdateStockProductService {
   constructor(
     private readonly useCase: UpdateStockProductUseCase,
     private readonly mapper: UpdateStockMapper,
   ) {}
 
-  async execute(request: UpdateStockDto): Promise<UpdateStockOutputDto> {
+  async execute(id: string, {amount}: UpdateStockDto): Promise<UpdateStockOutputDto> {
 
-    const input = this.mapper.toInput(request.id, request.amount);
+    const input = this.mapper.toInput(id, amount);
 
     const product = await this.useCase.execute(input);
 
