ac90d0d2508093d9ed35bce90894211c242eb82d
Leondvlpr <leondvlpr@gmail.com>
2026-01-31 18:52:36 -0500

    fix: fix ProductAdapterRepository clean unused imports and update body of update prisma product

--- src/products/infrastructure/adapters/product-adapter.repository.ts
+++ src/products/infrastructure/adapters/product-adapter.repository.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { ProductRepositoryPort } from 'src/products/application/ports/product.repository.port';
 import { Product as ProductEntity } from 'src/products/domain/entities/product.entity';
-import { Money } from 'src/products/domain/value-objects/money.vo';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ProductMapper } from '../mappers/product.mapper';
 
@@ -16,7 +14,6 @@ export class ProductAdapterRepository implements ProductRepositoryPort {
   private readonly products: ProductEntity[] = [];
 
   async findById(id: string): Promise<ProductEntity | null | undefined> {
-
     const product = await this.prisma.product.findUnique({
       where: {
         id,
@@ -33,7 +30,6 @@ export class ProductAdapterRepository implements ProductRepositoryPort {
   }
 
   async findAll(): Promise<ProductEntity[]> {
-
     const products = await this.prisma.product.findMany();
 
     if (!products) {
@@ -49,15 +45,17 @@ export class ProductAdapterRepository implements ProductRepositoryPort {
   }
 
   async update(product: ProductEntity): Promise<ProductEntity> {
-
     const productToUpdate = this.findById(product.getId());
 
     if (!productToUpdate) {
       throw new Error('No se puede actualizar un producto que no existe');
     }
 
-    const updateProductPrisma = this.productMapper.toPrisma(product);
-    
+    const updateProductPrisma = {
+      stock: product.getStock(),
+      updatedAt: new Date(),
+    };
+
     const updated = await this.prisma.product.update({
       where: {
         id: product.getId(),
