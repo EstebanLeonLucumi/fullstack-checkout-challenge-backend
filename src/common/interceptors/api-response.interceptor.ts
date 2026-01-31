fa830c83e3959fa713816fc8b50d474f10d1b91b
Leondvlpr <leondvlpr@gmail.com>
2026-01-31 18:47:06 -0500

    fix: correct ApiResponseInterceptor to handle status messages properly

--- src/common/interceptors/api-response.interceptor.ts
+++ src/common/interceptors/api-response.interceptor.ts
@@ -1,5 +1,6 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
 import { map, Observable } from "rxjs";
+import { Messages } from "../utils/http-messages";
 
 @Injectable()
 export class ApiResponseInterceptor implements NestInterceptor {
@@ -7,12 +8,48 @@ export class ApiResponseInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 
         return next.handle().pipe(
-            map((data) => ({
-                message: 'Success',
-                data,
-                timestamp: new Date(),
-            }))
-        );
+            map((result) => {
+
+                const ctx = context.switchToHttp();
+                const response = ctx.getResponse();
+                const statusCode = response.statusCode;
+
+                const { data, operation } = result || {};
+
+                let message = 'Request processed';
+
+                if (operation && Messages[operation]) {
+                    message = Messages[operation];
+                } else {
+
+                    switch (statusCode) {
+                        case 200:
+                            message = Messages.PRODUCT_CREATED;
+                            break;
+                        case 201:
+                            message = Messages.PRODUCT_CREATED;
+                            break;
+                        case 204:
+                            message = Messages.PRODUCT_DELETED;
+                            break;
+                        case 200:
+                            message = Messages.STOCK_UPDATED;
+                            break;
+                        case 404:
+                            message = Messages.PRODUCT_NOT_FOUND;
+                            break;
+                    }
+
+                }
+
+                return {
+                    statusCode,
+                    message,
+                    data: data ?? result,
+                    timestamp: new Date(),
+                };
+            })
+        )
     }
 
 }
